8aa656ff7e87e542ed45c3c366c9f1406534b62e
RyanNeff <ryan.ray.neff@fluke.com>
2022-04-27 10:08:57 -0700

    Moved store and delete from string to obj

--- app.js
+++ app.js
@@ -35,18 +35,24 @@ function addTask(e) {
   const li = document.createElement('li');
   // Add class
   li.className = "collection-item";
+  
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
+  
   // Create new link element
   const link = document.createElement('a');
   // Add class 
   link.className = 'delete-item secondary-content';
   // Add icon html 
   link.innerHTML = '<i class="fa fa-remove"></i>';
+
+
   // Append the link to the li
   li.appendChild(link);
+  
   // Append li to ul
   taskList.appendChild(li);
+  
   // Store in LS
   storeTaskInLocalStorage(taskInput.value);
   //clear input
@@ -66,21 +72,29 @@ function removeTask(e) {
   }
 }
 
+// Remove task from storage.
 function removeTaskFromLocalStorage(taskItem) {
-  let tasks;
-  if (localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  } 
-  tasks.forEach(function(task, index) {
+  // Get tasks from LS.
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  // Remove task from Array.
+  tasks.forEach((task, index) => {
     if (taskItem.textContent === task) {
       tasks.splice(index, 1);
-
     }
   });
   localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  // Get tasks from LS.
+  let tasksObj = JSON.parse(localStorage.getItem('tasksObj')) || {};
+  // Remove task.
+  if (tasksObj.hasOwnProperty(taskItem.textContent)) {
+    delete tasksObj[taskItem.textContent];
+  }
+
+  // Set back to local storage.
+  localStorage.setItem('tasksObj', JSON.stringify(tasksObj));
 }
+
 // Clear tasks
 function clearTasks() {
   // Kill it all
@@ -119,7 +133,7 @@ function filterTasks(e) {
 
 // Get tasks from LS
 // @todo - inside the foreach is duplicated - DRY
-function  getTasks() {
+function getTasks() {
   let tasks;
   if (localStorage.getItem('tasks') === null) {
     tasks = [];
@@ -145,28 +159,28 @@ function  getTasks() {
     taskList.appendChild(li);
   })
 }
+
 // Store in LS
 function storeTaskInLocalStorage(task) {
-  
-  // if(localStorage.getItem('tasks') === null) {
-  //   tasks = [];
-  // } else {
-  //   tasks = JSON.parse(localStorage.getItem('tasks'));
-  // }
+  // Get tasks from LS.
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-   console.log(`tasks ${tasks} task:${task}`);
-
-  // taskObj = {}; 
-  // taskObj[task] = { 
-  //   'timestamp': new Date().getTime(),
-  //   'priority' : 1,
-  // }
-
   // Add the new task
   tasks.push(task);
-
   // Set back to local storage
   localStorage.setItem('tasks', JSON.stringify(tasks));
-}
 
-// 
+  // Get tasks from LS.
+  let tasksObj = JSON.parse(localStorage.getItem('tasksObj')) || {};
+  // Add the new task.
+  tasksObj[task] = { 
+    'created': new Date().getTime(),
+    'sequence' : 1,
+    'reoccuring' : 0,
+    'time-estimate': 0,
+    'tags': [],
+  }
+  // Set back to local storage.
+  // @todo: post to Google sheets API for portability.
+  localStorage.setItem('tasksObj', JSON.stringify(tasksObj));
+  // console.dir(tasksObj);
+}
